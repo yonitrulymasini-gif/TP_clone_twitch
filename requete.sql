a534c6dfa90fd463796ad8674d990d6bea5f74ca
yonitrulymasini-gif <yonitrulymasini@gmail.com>
2025-12-01 19:22:25 +0100

    Ajout de la 3eme requete

--- requete.sql
+++ requete.sql
@@ -8,3 +8,11 @@ LIMIT 3;
 
 2eme Requete : Afficher les utilisateurs majeurs (18 ans et plus).
 SELECT * FROM user WHERE date_naissance <= DATE_SUB(CURDATE(), INTERVAL 18 YEAR)
+
+3eme Requete : Afficher le nombre de messages postés par chaque utilisateur.
+SELECT u.pseudo, COUNT(c.id_chat) AS "Nombre de messages"
+FROM user u
+JOIN chat c
+ON u.id_user = c.id_user
+GROUP BY u.pseudo;
+...
