b8a3e322d88c3224fa007a860a2d00d2f7a26682
yonitrulymasini-gif <yonitrulymasini@gmail.com>
2025-12-02 08:52:40 +0100

    modification de la 1er requete

--- requete.sql
+++ requete.sql
@@ -1,5 +1,5 @@
 1er Requete : Afficher les 3 streameurs ayant le plus de viewers.
-SELECT c.viewer, r.streameur AS "Streameur"
+SELECT c.viewer, nom_role AS "Streameur"
 FROM chat c
 JOIN role r
 ON r.id_role = c.id_role
